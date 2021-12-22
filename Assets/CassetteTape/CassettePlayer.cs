1941f0cc4d59ccfee07967788f91c60f4f30c5b2
TakashiHamada <hamada@gift10.net>
2021-12-23 02:07:03 +0900

    不具合修正、見た目調整

--- Assets/CassetteTape/CassettePlayer.cs
+++ Assets/CassetteTape/CassettePlayer.cs
@@ -6,75 +6,76 @@ using UnityEngine;
 
 public class CassettePlayer : MonoBehaviour
 {
-    [SerializeField] private AudioClip[] clips;
+    [SerializeField] private AudioClip clip;
     [SerializeField] private TextMeshProUGUI timeIndicator;
-
     private AudioSource player;
-    private bool isRewind = false;
-    private bool isSppedyRewind = false;
 
     [SerializeField] private CassetteTapeButtons buttons;
 
     void Start()
     {
+        var SPEEDY = 20f;
+        var SLOW = 4;
+        
         this.player = GetComponent<AudioSource>();
-        this.player.clip = this.clips[0];
         this.player.pitch = 0f;
         this.player.Play();
 
+        // カセットテープの挿入
         Observable.EveryUpdate()
             .Select(_ => this.buttons.CanReadCassetteTape)
             .DistinctUntilChanged()
-            .Subscribe(f => this.player.clip = this.clips[f ? 0 : 1])
+            .Subscribe(f => this.player.clip = f ? clip : null)
             .AddTo(this);
 
+        var onPlay = Observable.EveryFixedUpdate()
+            .Where(_ => this.player.clip != null)
+            .Publish().RefCount();
+
         // 再生ボタン
-        Observable.EveryUpdate()
-            .Select(_ => this.buttons.IsBtnPlay)
+        onPlay.Select(_ => this.buttons.IsBtnPlay)
             .DistinctUntilChanged()
             .Skip(1) // 起動の一発目は無視
             .Subscribe(isPlay => SetPlayMode(isPlay)) // 停止
             .AddTo(this);
 
         // 一時停止ボタン
-        Observable.EveryUpdate()
-            .Select(_ => this.buttons.IsBtnPause)
+        onPlay.Select(_ => this.buttons.IsBtnPause)
             .DistinctUntilChanged()
             .Skip(1) // 起動の一発目は無視
             .Subscribe(isPause => SetPlayMode(!isPause))
             .AddTo(this);
 
         // 巻き戻しボタン
-        Observable.EveryFixedUpdate()
-            .Select(_ => this.buttons.IsBtnRewind)
+        onPlay.Select(_ => this.buttons.IsBtnRewind)
             .Where(f => f)
-            .Subscribe(isBtnRewind => { this.player.pitch = this.buttons.IsBtnPlay ? -4 : -40; })
+            .Subscribe(isBtnRewind => this.player.pitch = this.buttons.IsBtnPlay ? -SLOW : -SPEEDY)
             .AddTo(this);
 
         // 早送りボタン
-        Observable.EveryFixedUpdate()
-            .Select(_ => this.buttons.IsBtnForward)
+        onPlay.Select(_ => this.buttons.IsBtnForward)
             .Where(f => f)
-            .Subscribe(isBtnForward => { this.player.pitch = this.buttons.IsBtnPlay ? 4 : 40; })
+            .Subscribe(isBtnForward => this.player.pitch = this.buttons.IsBtnPlay ? SLOW : SPEEDY)
             .AddTo(this);
 
-        // ピッチを戻す
-        Observable.EveryFixedUpdate()
-            .Select(_ => this.buttons)
-            .Where(b => !b.IsBtnForward && !b.IsBtnRewind)
+        // 巻き戻しと早送りが離されたとき
+        onPlay.Select(_ => this.buttons)
+            .Select(b => !b.IsBtnForward && !b.IsBtnRewind)
             .DistinctUntilChanged()
-            .Subscribe(_ => { SetPlayMode(this.buttons.IsBtnPlay); })
+            .Where(f => f)
+            .Subscribe(_ => SetPlayMode(this.buttons.IsBtnPlay))
             .AddTo(this);
 
-        Observable.EveryFixedUpdate()
-            .Select(_ => this.player.isPlaying)
+        // 限界判定
+        onPlay.Select(_ => this.player.isPlaying)
             .DistinctUntilChanged()
             .Where(f => !f)
             // なぜかピッチをマイナスにして0を下回ると、最大値になる
             .Where(_ => this.player.time >= this.player.clip.length)
             .Subscribe(_ =>
             {
-                this.player.time = this.buttons.IsBtnRewind ? 0f
+                this.player.time = this.buttons.IsBtnRewind
+                    ? 0f
                     : this.player.clip.length - 0.001f; // ちょうどだとエラー
 
                 this.player.pitch = 0;
@@ -86,22 +87,17 @@ public class CassettePlayer : MonoBehaviour
 
         Observable.EveryUpdate()
             .Select(_ => this.timeIndicator)
-            .Subscribe(target => target.text = this.player.time.ToString("F2"))
+            .Subscribe(target => target.text =
+                this.player.clip == null ? "-- --" : this.player.time.ToString("F2"))
             .AddTo(this);
     }
 
     private void SetPlayMode(bool isPlay)
     {
-        if (isPlay && !this.buttons.IsBtnPause)
-        {
-            this.player.pitch = 1f;
-            this.player.volume = 1f;
-        }
-        else
-        {
-            this.player.pitch = 0f;
-            this.player.volume = 0f;
-        }
+        var playable = isPlay && !this.buttons.IsBtnPause;
+
+        this.player.pitch = playable ? 1f : 0f;
+        this.player.volume = playable ? 1f : 0f;
 
         if (!this.player.isPlaying)
             this.player.Play();
