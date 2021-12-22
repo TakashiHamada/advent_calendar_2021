feebfcd5d7c2f209b1fc7d2aaaf27bb245875b0f
TakashiHamada <hamada@gift10.net>
2021-12-23 01:27:14 +0900

    だいたい動くようになる

--- Assets/CassetteTape/CassettePlayer.cs
+++ Assets/CassetteTape/CassettePlayer.cs
@@ -17,21 +17,6 @@ public class CassettePlayer : MonoBehaviour
 
     void Start()
     {
-        Observable.EveryUpdate()
-            .Select(_ => this.buttons.IsBtnPlay)
-            .DistinctUntilChanged()
-            .Skip(1) // 起動の一発目は無視
-            .Subscribe(isPlay => SetPlayMode(isPlay))
-            .AddTo(this);
-
-        Observable.EveryUpdate()
-            .Select(_ => this.buttons.IsBtnPause)
-            .DistinctUntilChanged()
-            .Skip(1) // 起動の一発目は無視
-            .Subscribe(isPause => SetPlayMode(!isPause))
-            .AddTo(this);
-
-
         this.player = GetComponent<AudioSource>();
         this.player.clip = this.clips[0];
         this.player.pitch = 0f;
@@ -43,12 +28,30 @@ public class CassettePlayer : MonoBehaviour
             .Subscribe(f => this.player.clip = this.clips[f ? 0 : 1])
             .AddTo(this);
 
+        // 再生ボタン
+        Observable.EveryUpdate()
+            .Select(_ => this.buttons.IsBtnPlay)
+            .DistinctUntilChanged()
+            .Skip(1) // 起動の一発目は無視
+            .Subscribe(isPlay => SetPlayMode(isPlay)) // 停止
+            .AddTo(this);
+
+        // 一時停止ボタン
+        Observable.EveryUpdate()
+            .Select(_ => this.buttons.IsBtnPause)
+            .DistinctUntilChanged()
+            .Skip(1) // 起動の一発目は無視
+            .Subscribe(isPause => SetPlayMode(!isPause))
+            .AddTo(this);
+
+        // 巻き戻しボタン
         Observable.EveryFixedUpdate()
             .Select(_ => this.buttons.IsBtnRewind)
             .Where(f => f)
             .Subscribe(isBtnRewind => { this.player.pitch = this.buttons.IsBtnPlay ? -4 : -40; })
             .AddTo(this);
 
+        // 早送りボタン
         Observable.EveryFixedUpdate()
             .Select(_ => this.buttons.IsBtnForward)
             .Where(f => f)
@@ -59,45 +62,48 @@ public class CassettePlayer : MonoBehaviour
         Observable.EveryFixedUpdate()
             .Select(_ => this.buttons)
             .Where(b => !b.IsBtnForward && !b.IsBtnRewind)
+            .DistinctUntilChanged()
             .Subscribe(_ => { SetPlayMode(this.buttons.IsBtnPlay); })
             .AddTo(this);
 
-        // ボタンリセット
         Observable.EveryFixedUpdate()
-            .Select(_ => Playable())
+            .Select(_ => this.player.isPlaying)
             .DistinctUntilChanged()
-            .Where(f => f)
-            .Subscribe(_ => this.buttons.Reset())
+            .Where(f => !f)
+            // なぜかピッチをマイナスにして0を下回ると、最大値になる
+            .Where(_ => this.player.time >= this.player.clip.length)
+            .Subscribe(_ =>
+            {
+                this.player.time = this.buttons.IsBtnRewind ? 0f
+                    : this.player.clip.length - 0.001f; // ちょうどだとエラー
+
+                this.player.pitch = 0;
+                this.player.Play(); // Playしないとtimeが更新されない
+
+                this.buttons.Reset();
+            })
             .AddTo(this);
 
         Observable.EveryUpdate()
             .Select(_ => this.timeIndicator)
-            .Subscribe(target => target.text = this.player.time.ToString("F1"))
+            .Subscribe(target => target.text = this.player.time.ToString("F2"))
             .AddTo(this);
     }
 
-    private bool Playable()
-    {
-        var t = this.player.time;
-        return t <= 0 || this.player.clip.length - 1f <= t;
-    }
-
-
     private void SetPlayMode(bool isPlay)
     {
-        var player2 = GetComponent<AudioSource>();
         if (isPlay && !this.buttons.IsBtnPause)
         {
-            player2.pitch = 1f;
-            player2.volume = 1f;
+            this.player.pitch = 1f;
+            this.player.volume = 1f;
         }
         else
         {
-            player2.pitch = 0f;
-            player2.volume = 0f;
+            this.player.pitch = 0f;
+            this.player.volume = 0f;
         }
 
-        if (!player2.isPlaying)
-            player2.Play();
+        if (!this.player.isPlaying)
+            this.player.Play();
     }
 }
